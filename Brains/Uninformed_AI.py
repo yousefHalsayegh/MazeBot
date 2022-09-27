671ecc8074b5bdf207d312685686cbb29dedb313
Yousef-Alsayegh <96050534+yousefHalsayegh@users.noreply.github.com>
2022-09-27 21:04:03 +0300

    Create Uninformed_AI.py

---
+++ Brains/Uninformed_AI.py
@@ -0,0 +1,18 @@
+class BFS:
+
+    def __init__(self) -> None:
+        pass
+
+
+class DFS:
+
+    def __init__(self) -> None:
+        pass
+
+class DLS:
+    def __init__(self) -> None:
+        pass
+
+class IDS:
+    def __init__(self) -> None:
+        pass
